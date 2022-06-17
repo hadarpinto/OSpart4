fbdde41172ca8759eb7612df61b78b408e4c2183
hadar <hadarpinto@gmail.com>
2022-06-17 21:52:30 +0300

    works!!!

--- ex4_client.c
+++ ex4_client.c
@@ -1,36 +1,22 @@
 //
 // Created by Hadar Pinto 316460146
 //
-//#include <stdio.h>
-//#include <stdlib.h>
-//#include <string.h>
-//#include <signal.h>
-//#include <sys/types.h>
-//#include <unistd.h>
-//#include <dirent.h>
-//#include <sys/wait.h>
-//#include <sys/stat.h>
-//#include <fcntl.h>
-//#include <sys/random.h>
-//#include <time.h>
 #include <stdio.h>
 #include <fcntl.h>
 #include <unistd.h>
 #include <stdlib.h>
 #include <string.h>
-
 #include <signal.h>
 #include <linux/random.h>
 #include <sys/random.h>
 
-void test(int s);
+
 void signalHandler(int sig);
 void alarmHandler(int sig);
 void readLineToArr(char* buf, char* arr, int fd);
 int main(int argc, char **argv) {
     int serverID;
     serverID = atoi(argv[1]);
-    printf("%d my pid\n", getpid());
 
     signal(SIGALRM, alarmHandler);
     signal(SIGUSR2,signalHandler);
@@ -39,7 +25,6 @@ int main(int argc, char **argv) {
     int j;
     for(j = 1; j <= 10; j++){
         int randomWait = rand() % 6;
-        printf("%d\n", randomWait);
         sleep(randomWait);
         //make file to_srv
         int fd = open("to_srv.txt", O_CREAT | O_WRONLY | O_RDONLY | O_EXCL, 0777);
@@ -60,7 +45,6 @@ int main(int argc, char **argv) {
             char line[255];
             sprintf(line, "%s\n", argv[i]);
             write(fd, line, strlen(line));
-
         }
         break;
 
@@ -73,97 +57,32 @@ int main(int argc, char **argv) {
 
 
 }
-void test(int s) {
-  //  alarm(30);
-    printf("been here............\n");
-    char fileAnswer[255]="";
-    char answer[255], *byte_buf;
-    //strcpy(fileAnswer, "aa.txt");
 
-    printf("filename is %s\n", fileAnswer);
-
-    int fdAnswer = open("aa.txt", O_RDWR | O_APPEND | O_CREAT, 0777);
-    char r = 'a';
-    long kk;
-    kk =  read(fdAnswer, &r, 1);
-
-        while (r != '\n'){
-            printf("%c", r);
-            kk =  read(fdAnswer, &r, 1);
-        }
-
-
-
-    close(fdAnswer);
-    remove(fileAnswer);
-    exit(0);
-}
 void signalHandler(int sig){
-    //alarm(30);
-    printf("been here............\n");
+    alarm(30);
     char fileAnswer[255];
-    char answer[255], *byte_buf;
+    char answer[255];
     strcpy(fileAnswer, "to_client_");
     sprintf(fileAnswer, "%s%d", fileAnswer,getpid());
     strcat(fileAnswer,".txt");
-    printf("filename is %s\n", fileAnswer);
 
     int fdAnswer = open(fileAnswer, O_RDWR | O_APPEND | O_CREAT, 0777);
     char d = 'a';
     int kk;
     kk =  read(fdAnswer, &d, 1);
-        while (d != '\n'){
-            printf("%c", d);
-            read(fdAnswer, &d, 1);
-        }
-
-
+    while (d != '\n') {
+        printf("%c", d);
+        read(fdAnswer, &d, 1);
+    }
+    printf("\n");
 
     close(fdAnswer);
     remove(fileAnswer);
-    exit(0);
-
-
-
-//    printf("filename is %d\n", fdAnswer);
-//    strcpy(answer,"hey\0");
-//    printf("Answer is: %s\n", answer);
-//    printf("im doing trouble when i equal too\n");
-//    printf("filename is %s\n", fileAnswer);
-//    //readLineToArr(byte_buf,answer,fdAnswer);
-//
-//    printf("im doing trouble when i equal too ");
-//    int i=0, byte;
-//    byte = read(fdAnswer,&byte_buf,1);
-//
-//    while (byte_buf != '\n' || byte == -1) {
-//        answer[i++] = byte_buf;
-//        byte = read(fdAnswer,&byte_buf,1);
-//    }
-//    answer[i] = '\0';
-//
-//
-//    printf("Answer is: %s\n", answer);
-//    close(fdAnswer);
-    //remove(fileAnswer);
-
+    exit(1);
 }
 
 void alarmHandler(int sig){
     printf("Client closed because no response was received from the server for 30 seconds\n");
     exit(1);
-
-}
-
-void readLineToArr(char* buf, char* arr, int fd) {
-    printf("im doing trouble when i equal too ");
-    int i=0, byte;
-    byte = read(fd,&buf,1);
-
-    while (buf != '\n' || byte == -1) {
-        arr[i++] = buf;
-        byte = read(fd,&buf,1);
-    }
-    arr[i] = '\0';
 
 }
--- ex4_srv.c
+++ ex4_srv.c
@@ -13,8 +13,7 @@
 #include <fcntl.h>
 #include <sys/random.h>
 #include <time.h>
-
-void test(int s);
+void test();
 void signalHandler (int signal);
 void alarmHandler (int signal);
 void readLineToArr(char* buf, char* arr, int fd);
@@ -23,33 +22,36 @@ int calculate(int num1, int operation, int num2);
 
 int main(int argc, char **argv) {
     signal(SIGUSR1, signalHandler);
+    signal(SIGALRM, alarmHandler);
     alarm(60);
     while(1) {
         printf("Listening.......\n");
         pause();
-
+        signal(SIGUSR1, signalHandler);
+        alarm(60);
     }
+
 }
 
-void test(int s){
-    int fdToSrvFile = open("to_srv.txt",O_RDONLY, 0777);
+void test() {
+    printf("in son\n");
+    //takecare of files
+    int fdToSrvFile = open("to_srv.txt",O_RDWR | O_APPEND | O_CREAT, 0777);
     char clientPID[255]="", num1Char[255]="", operationChar[255]="", num2Char[255]="", *byteBuf, answerChar[255]="",toClientFileName[255]="";
+
     readLineToArr(byteBuf, clientPID, fdToSrvFile);
-
     readLineToArr(byteBuf, num1Char, fdToSrvFile);
-
     readLineToArr(byteBuf, operationChar, fdToSrvFile);
-
     readLineToArr(byteBuf, num2Char, fdToSrvFile);
 
-    if (!isDivisionByZero(operationChar, num2Char)) {
-        strcpy(answerChar, "CANNOT_DIVIDE_BY_ZERO\n");
 
-        exit(1);
+    if((!strcmp("4",operationChar)) && (!strcmp("0",num2Char))){
+        printf("division");
+        printf(" by zero\n");
+        strcpy(answerChar, "CANNOT_DIVIDE_BY_ZERO\n");
     }
-        //not a division by zero, assign answerChar with answer
     else {
-        printf("calculting....\n");
+        printf("not division\n");
         int num1Int, num2Int, operationInt, answerInt;
         num1Int = atoi(num1Char);
         operationInt = atoi(operationChar);
@@ -65,50 +67,39 @@ void test(int s){
 
 
     int fdClientFile = open(toClientFileName, O_CREAT | O_RDWR, 0777);
-    printf("%s", toClientFileName);
-    int writeint = write(fdClientFile, answerChar, strlen(answerChar));
-    printf("%d writeint \n",writeint);
-    //close(fdClientFile);
+    write(fdClientFile, answerChar, strlen(answerChar));
+    close(fdClientFile);
     //send signal to client
+    printf("send singal to client\n");
     int client = atoi(clientPID);
-    printf("im at bottom handler %d\n",client);
     kill(client, SIGUSR2);
     remove("to_srv.txt");
-
-
-
-
 }
 void signalHandler (int sig){
-    signal(SIGUSR1, signalHandler);
-    alarm(60);
-    printf("in signalHandler\n");
-
     int pid;
     pid = fork();
     if (pid < 0){
         exit(1);
     }
     else if(pid == 0){
+        printf("in son\n");
         //takecare of files
         int fdToSrvFile = open("to_srv.txt",O_RDWR | O_APPEND | O_CREAT, 0777);
         char clientPID[255]="", num1Char[255]="", operationChar[255]="", num2Char[255]="", *byteBuf, answerChar[255]="",toClientFileName[255]="";
+
         readLineToArr(byteBuf, clientPID, fdToSrvFile);
-
         readLineToArr(byteBuf, num1Char, fdToSrvFile);
-
         readLineToArr(byteBuf, operationChar, fdToSrvFile);
-
         readLineToArr(byteBuf, num2Char, fdToSrvFile);
 
-        if (!isDivisionByZero(operationChar, num2Char)) {
-            strcpy(answerChar, "CANNOT_DIVIDE_BY_ZERO\n");
 
-            exit(1);
+        if((!strcmp("4",operationChar)) && (!strcmp("0",num2Char))){
+            printf("division");
+            printf(" by zero\n");
+            strcpy(answerChar, "CANNOT_DIVIDE_BY_ZERO\n");
         }
-            //not a division by zero, assign answerChar with answer
         else {
-            printf("calculting....\n");
+            printf("not division\n");
             int num1Int, num2Int, operationInt, answerInt;
             num1Int = atoi(num1Char);
             operationInt = atoi(operationChar);
@@ -124,33 +115,34 @@ void signalHandler (int sig){
 
 
         int fdClientFile = open(toClientFileName, O_CREAT | O_RDWR, 0777);
-        printf("%s", toClientFileName);
-        int writeint = write(fdClientFile, answerChar, strlen(answerChar));
-        printf("%d writeint \n",writeint);
-        //close(fdClientFile);
+        write(fdClientFile, answerChar, strlen(answerChar));
+        close(fdClientFile);
         //send signal to client
+        printf("send singal to client\n");
         int client = atoi(clientPID);
-        printf("im at bottom handler %d\n",client);
         kill(client, SIGUSR2);
         remove("to_srv.txt");
+
     }
+    else {
+
+    }
+
+
 
 }
 
 void readLineToArr(char* buf, char* arr, int fd) {
     int i=0, byte;
     byte = read(fd,&buf,1);
-    printf("%d byte\n", byte);
-    while (buf != '\n' || byte == -1 || byte == 0) {
+    while (buf != '\n') {
         arr[i++] = buf;
         byte = read(fd,&buf,1);
-        printf("%d byte\n", byte);
     }
     arr[i] = '\0';
-    printf("FAiled\n");
 }
 int calculate(int num1, int operation, int num2){
-    printf("in calculate\n");
+    int sum;
     if (operation == 1){
         return num1 + num2;
     }
@@ -158,19 +150,22 @@ int calculate(int num1, int operation, int num2){
         return num1 - num2;
     }
     else if (operation == 3){
-        return num1 * num2;
+        sum = num1*num2;
+        return sum;
     }
     else if (operation == 4) {
-        return num1 / num2;
+        sum = num1/num2;
+        return sum;
     }
 }
 void alarmHandler(int sig){
     printf("The server was closed because no service request was received for the last 60 seconds\n");
+    while(wait(NULL) != -1){}
     exit(1);
 
 }
 int isDivisionByZero(char* operation, char* num2){
-    if(strcmp(operation,"4") && strcmp(num2, "0")){
+    if(!strcmp(operation,"4") && !strcmp(num2, "0")){
         return 1;
     }
     return 0;
